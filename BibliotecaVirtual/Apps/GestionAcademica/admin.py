f71541261185dec786444cd4c3972d277e99fcb0
Israel Edgar Gonzales Palomino <igonzalesp@uni.pe>
2018-11-06 10:53:35 -0500

    Initial commit

---
+++ BibliotecaVirtual/Apps/GestionAcademica/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from BibliotecaVirtual.Apps.GestionAcademica.models import *
+# Register your models here.
+
+
+admin.site.register(Alumno)
+
+admin.site.register(Curso)
+
+admin.site.register(Matricula)
